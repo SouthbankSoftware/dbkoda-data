53610bee924a50d83f590ddd8f4e3836f91c1c46
Guy Harrison <guy@southbanksoftware.com>
2017-08-29 16:25:12 +1000

    Update load_atlas.sh

--- SampleCollections/load_atlas.sh
+++ SampleCollections/load_atlas.sh
@@ -1,5 +1,5 @@
 export MONGOURL="mongodb://cluster0-shard-00-00-oexhg.mongodb.net:27017,cluster0-shard-00-01-oexhg.mongodb.net:27017,cluster0-shard-00-02-oexhg.mongodb.net:27017/admin?replicaSet=Cluster0-shard-0"
 export MONGOHOST="--host Cluster0-shard-0/cluster0-shard-00-00-oexhg.mongodb.net:27017,cluster0-shard-00-01-oexhg.mongodb.net:27017,cluster0-shard-00-02-oexhg.mongodb.net:27017" 
-export OPTIONS='--ssl --username dbenvy --password DBEnvy@2016 --authenticationDatabase=admin'
+export OPTIONS='--ssl --username dbenvy --password xxxxxx --authenticationDatabase=admin'
 echo mongorestore  $MONGOURL $OPTIONS --drop
 mongorestore $MONGOHOST $OPTIONS --drop
